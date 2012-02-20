6524427f4dfd2da9d2032388a3749004dbf0c494
Blair Vanderhoof <blairv@gmail.com>
2012-02-20 00:13:31 -0800

    First commit for embed gist

---
+++ gist-embed.js
@@ -0,0 +1,27 @@
+$(function(){
+	//find all elements with gistEmbed class
+	$(".gistEmbed").each(function(){
+		var $elem, id;
+		$elem = $(this);
+		id = $elem.attr("id");
+		id = id || "";
+		//get the numeric id from the id attribute of the element holder
+		id = id.replace("gist-","");
+
+		//make sure result is a numeric id
+		if(!isNaN(parseInt(id,10))){
+			//request the json version of this gist
+			$.ajax({ 
+				url: "https://gist.github.com/"+id+".json", 
+				dataType: "jsonp", 
+				success: function(response){
+					//the html payload is in the div property
+					if(response && response.div){
+						//add the html to your element holder
+						$elem.html(response.div);
+					}
+				}
+			});	
+		}	
+	});
+});
